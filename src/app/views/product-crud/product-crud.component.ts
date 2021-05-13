7a135403513b6ad30b32fcb29289645f233bca70
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2021-05-12 22:43:52 -0300

    Event Binding

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product-crud',
   templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+  styleUrls: ['./product-crud.component.css'],
 })
 export class ProductCrudComponent implements OnInit {
+  coolAttribute = 'anything';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  doSomething(): void {
+    console.log('Doing something');
   }
-
 }
